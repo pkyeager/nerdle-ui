12576ac92a13c3ed380412fa8c35dbaa429a0ffd
phkon23 <phkon23@student.sdu.dk>
2024-12-06 08:40:52 +0100

    chore: try to get github pages with storybook running

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,11 +1,15 @@
 module.exports = {
-  stories: ['../src/**/*.stories.@(js|jsx|ts|tsx)'],
-  addons: ['@storybook/addon-essentials'],
-  framework: {
-    name: '@storybook/html-webpack5',
-    options: {}
-  },
-  docs: {
-    autodocs: true
-  }
-}; 
+    stories: ['../src/**/*.stories.@(js|jsx|ts|tsx)'],
+    addons: [
+      '@storybook/addon-links',
+      '@storybook/addon-essentials',
+      '@storybook/addon-interactions'
+    ],
+    framework: {
+      name: '@storybook/html-webpack5',
+      options: {}
+    },
+    docs: {
+      autodocs: true
+    }
+  }; 
--- src/stories/Button.stories.ts
+++ src/stories/Button.stories.ts
@@ -1,27 +1,51 @@
 import { Button } from '../components/Button/Button';
 
 export default {
-    title: 'Components/Button',
-    argTypes: {
-        text: { control: 'text' },
-        variant: {
-            control: { type: 'select' },
-            options: ['primary', 'secondary', 'success', 'danger']
-        },
-        size: {
-            control: { type: 'select' },
-            options: ['sm', 'md', 'lg']
-        }
+  title: 'Components/Button',
+  tags: ['autodocs'],
+  argTypes: {
+    text: { control: 'text' },
+    variant: {
+      control: { type: 'select' },
+      options: ['primary', 'secondary', 'success', 'danger']
+    },
+    size: {
+      control: { type: 'select' },
+      options: ['sm', 'md', 'lg']
     }
+  }
+};
+
+const Template = (args: any) => {
+  const button = new Button({
+    text: args.text || 'Click me',
+    variant: args.variant || 'primary',
+    size: args.size || 'md',
+    onClick: () => console.log('Button clicked!')
+  });
+  return button.render();
 };
 
 export const Primary = {
-    render: (args: any) => {
-        const button = new Button({
-            text: args.text || 'Click me',
-            variant: args.variant,
-            size: args.size
-        });
-        return button.render();
-    }
+  render: Template,
+  args: {
+    text: 'Primary Button',
+    variant: 'primary'
+  }
+};
+
+export const Secondary = {
+  render: Template,
+  args: {
+    text: 'Secondary Button',
+    variant: 'secondary'
+  }
+};
+
+export const Small = {
+  render: Template,
+  args: {
+    text: 'Small Button',
+    size: 'sm'
+  }
 }; 
